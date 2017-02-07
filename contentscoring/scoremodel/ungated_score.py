c2746ac0b269cdc87d3be5d23825e4a47b595d36
pramod toraskar <ptoraska@redhat.com>
2017-02-07 16:20:38 +0530

    Update ungated score model - Add ungated score calculation from sub score models

--- contentscoring/scoremodel/ungated_score.py
+++ contentscoring/scoremodel/ungated_score.py
@@ -1,3 +1,42 @@
 
+
+from contentscoring.scoresubmodel import completion_score
+from contentscoring.scoresubmodel import recency_score
+from contentscoring.scoresubmodel import trajectory_score
+from contentscoring.scoresubmodel import volume_score
+
+
+def get_completion_score():
+    return 0
+
+
+def get_recency_score():
+    return 0
+
+
+def get_trajectory_score():
+    return 0
+
+
+def get_volume_score():
+    return 0
+
+
 def ungated_score():
-    pass
+    """
+
+    :return:
+    """
+
+    completion_score = get_completion_score()
+
+    recency_score = get_recency_score()
+
+    trajectory_score = get_trajectory_score()
+
+    volume_score = get_volume_score()
+
+    un_gated_score = ((completion_score * 0.35) + (recency_score * 0.45) + (trajectory_score * 0.10)
+                      + (volume_score * 0.10))
+
+    return un_gated_score
